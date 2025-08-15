0023a37522662daf8c6246f1dd4357ed684c06c6
Gaga <gaetan.chantebien@gmail.com>
2025-08-14 23:44:05 -0400

    r

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -3,7 +3,6 @@
  */
 
 // Export types
-export * from './applydiff/coreapplydifftypes.js';
 export * from './fusion/corefusiontypes.js';
 
 // Export schemas
@@ -14,6 +13,3 @@ export * from './coreutils.js';
 
 // Export fusion functionality
 export { processFusion } from './fusion/corefusion.js';
-
-// Export apply diff functionality
-export { processApplyDiff } from './applydiff/coreapplydiff.js';
